d10700118e6560b0987e637d305f8a55eb5b3404
Alexey Dodonov <dodonov_a_a@inbox.ru>
2016-05-13 17:19:59 +0300

    Common Mezon testing
    1. 1 unit-test was added

---
+++ tests/MezonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+    class MezonTest extends PHPUnit_Framework_TestCase
+    {
+        /**
+        *   Testing case when both GET and POST processors exists.
+        *   
+        */
+        public function testMezonIncludes()
+        {
+            $MEZON_PATH = dirname( __FILE__ ).'/../';
+            require_once( dirname( __FILE__ ).'/../mezon.php' );
+
+            //$this->expectOutputString( 'OK!' );
+        }
+    }
+
+?>
