2d5629388782c7a0cf689f972ad193904ee7d786
Dodonov <alexey@dodonov.pro>
2020-01-17 22:11:42 +0300

    bugfix/Unit-tests fixes

--- Service/Tests/ServiceBaseLogicUnitTest.php
+++ Service/Tests/ServiceBaseLogicUnitTest.php
@@ -1,5 +1,5 @@
 <?php
 
-class ServiceBaseLogicUnitTest extends \Mezon\Service\ServiceBaseLogic\ServiceBaseLogicUnitTests
+class ServiceBaseLogicUnitTest extends \Mezon\Service\Tests\ServiceBaseLogicUnitTests
 {
 }
