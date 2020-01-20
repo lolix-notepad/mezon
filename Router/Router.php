12a8fc8b2924a40b45f1f0e7a87abec1af849fc8
Dodonov <alexey@dodonov.pro>
2020-01-20 17:16:14 +0300

    feature/Small refactoring

--- Router/Router.php
+++ Router/Router.php
@@ -114,11 +114,11 @@ class Router
      * and $parameters is associative array (parameter name => parameter value) with URL parameters
      *
      * @param string $route
-     *            Route.
+     *            Route
      * @param mixed $callback
-     *            Collback wich will be processing route call.
+     *            Collback wich will be processing route call
      * @param string $request
-     *            Request type.
+     *            Request type
      */
     public function addRoute(string $route, $callback, $request = 'GET'): void
     {
